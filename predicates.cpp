4882d416c7a527b3b2425c490700fc070254c5df
Andre Masella <andre@masella.name>
2014-12-03 10:52:37 -0500

    Add is_paired node

--- predicates.cpp
+++ predicates.cpp
@@ -7,6 +7,22 @@
 namespace barf {
 
 /**
+ * A predicate that checks of the read is paired.
+ */
+class is_paired_node : public ast_node {
+	virtual llvm::Value *generate(llvm::Module *module, llvm::IRBuilder<> builder, llvm::Value *read) {
+		auto function = define_is_paired(module);
+		return builder.CreateCall(function, read);
+	}
+};
+
+static std::shared_ptr<ast_node> parse_is_paired(std::string input, int&index) throw (parse_error) {
+	static auto result = std::make_shared<is_paired_node>();
+	return result;
+}
+
+
+/**
  * A predicate that always return true.
  */
 class true_node : public ast_node {
@@ -25,6 +41,7 @@ static std::shared_ptr<ast_node> parse_true(std::string input, int&index) throw 
  */
 predicate_map getDefaultPredicates() {
 	return  {
+		{std::string("is_paired"), parse_is_paired},
 		{std::string("true"), parse_true}
 	};
 }
