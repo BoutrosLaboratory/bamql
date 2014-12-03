0ee92fc839b84b6bfd6308318707421aaa094d6f
Andre Masella <andre@masella.name>
2014-12-03 07:59:17 -0500

    Create predicate for true and create default predicate map

--- predicates.cpp
+++ predicates.cpp
@@ -2,3 +2,20 @@
 
 // This must be included exactly once in only this file!!!
 #include "runtime.inc"
+
+class true_node : public barf::ast_node {
+	virtual llvm::Value *generate(llvm::IRBuilder<> builder, llvm::Value *read) {
+		return llvm::ConstantInt::getTrue(llvm::getGlobalContext());
+	}
+};
+
+static std::shared_ptr<barf::ast_node> parse_true(std::string input, int&index) throw (barf::parse_error) {
+	static auto result = std::make_shared<true_node>();
+	return result;
+}
+
+barf::predicate_map barf::getDefaultPredicates() {
+	return  {
+		{std::string("true"), parse_true}
+	};
+}
