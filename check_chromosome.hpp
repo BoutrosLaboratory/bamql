c81993c6ad8b050ab29153ab48828484e0e3cab0
Andre Masella <andre.masella@oicr.on.ca>
2015-03-25 11:20:58 -0400

    Appease the lawyer cats

--- check_chromosome.hpp
+++ check_chromosome.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Paul Boutros. For details, see COPYING. Our lawyer cats sez:
+ *
+ * OICR makes no representations whatsoever as to the SOFTWARE contained
+ * herein.  It is experimental in nature and is provided WITHOUT WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE OR ANY OTHER WARRANTY,
+ * EXPRESS OR IMPLIED. OICR MAKES NO REPRESENTATION OR WARRANTY THAT THE USE OF
+ * THIS SOFTWARE WILL NOT INFRINGE ANY PATENT OR OTHER PROPRIETARY RIGHT.  By
+ * downloading this SOFTWARE, your Institution hereby indemnifies OICR against
+ * any loss, claim, damage or liability, of whatsoever kind or nature, which
+ * may arise from your Institution's respective use, handling or storage of the
+ * SOFTWARE. If publications result from research using this SOFTWARE, we ask
+ * that the Ontario Institute for Cancer Research be acknowledged and/or
+ * credit be given to OICR scientists, as scientifically appropriate.
+ */
+
 #include <htslib/sam.h>
 #include "barf.hpp"
 
