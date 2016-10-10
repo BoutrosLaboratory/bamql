8cd2bb79ee99202938b10a6973d846e5e4fb3ff8
Andre Masella <andre@masella.name>
2016-10-10 08:06:45 -0400

    Dump test suite log on failure

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,12 @@ set -eu
 autoreconf -i
 if ./configure --enable-static-llvm=${LLVM_STATIC} --with-llvm=/usr/bin/llvm-config-${LLVM_TARGET_VERSION}
 then
-	make all check
+	make all
+	if ! make check
+	then
+		cat test-suite.log
+		exit 1
+	fi
 else
 	cat config.log
 	exit 1
