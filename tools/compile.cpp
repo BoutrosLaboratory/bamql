6564bb7e94ec2e08f35cf2c1b3030a8618860eba
Andre Masella <andre@masella.name>
2018-03-26 15:58:51 -0400

    Fixes to support LLVM 6.0

--- tools/compile.cpp
+++ tools/compile.cpp
@@ -38,7 +38,6 @@
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/Target/TargetOptions.h>
-#include <llvm/Target/TargetSubtargetInfo.h>
 #include "bamql-compiler.hpp"
 #include "config.h"
 
