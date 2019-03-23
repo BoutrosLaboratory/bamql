d81f9b400f15f63237849e8fff3d9acb55523b8e
Andre Masella <andre@masella.name>
2019-03-23 09:03:29 -0400

    Fix Travis build

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,7 @@
 
 set -eu
 autoreconf -i
-if ./configure --enable-static-llvm=${LLVM_STATIC} --with-llvm=/usr/bin/llvm-config-${LLVM_TARGET_VERSION}
+if ./configure --enable-static-llvm=${LLVM_STATIC} --with-llvm-config=/usr/bin/llvm-config-${LLVM_TARGET_VERSION}
 then
 	make all
 	if ! make check
